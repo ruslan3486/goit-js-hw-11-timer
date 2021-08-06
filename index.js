42409c97c3451e7a26032d4d19e427391f369c35
Ruslan Kuzma <kuzrus31@gmail.com>
2021-08-06 19:45:53 +0300

    переделал ДЗ

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
+let refs = {
+    
+            days: document.querySelector('[data-value="days"]'),
+            hours: document.querySelector('[data-value="hours"]'),
+            mins: document.querySelector('[data-value="mins"]'),
+            secs: document.querySelector('[data-value="secs"]'),
+
+}
+
+
+
+
 class CountdownTimer {
 
-    constructor({ targetDate }) {
+    constructor({ targetDate, onTick, selector }) {
         
-        this.targetDate = targetDate,//установленный срок
-            this.days = document.querySelector('[data-value="days"]'),
-            this.hours = document.querySelector('[data-value="hours"]'),
-            this.mins = document.querySelector('[data-value="mins"]'),
-            this.secs = document.querySelector('[data-value="secs"]'),
+        this.onTick = onTick;
+        this.selector = selector;
+            
+        this.targetDate = targetDate;
             
         this.start();
        
     }
 
    
-    //Установка обратного таймера
+    
     start() {
- 
+        this.targetDate = new Date(this.targetDate).getTime();
          setInterval(() => {
 
 
-            let now = new Date().getTime();//текущее время
-            let t = this.targetDate.getTime() - now;//разница между установленным сроком и текущим временем
-            const timeStart = this.getTimeComponents(t);//вывод функции времени : день, час, минута, секунда
-             this.updateClockFace(timeStart);//вывод текстового контента времени
-
+            let now = new Date();//текущее время
+            let t = this.targetDate - now;
+            const timeStart = this.getTimeComponents(t);
+             this.onTick(timeStart);
+            
+             
         }, 1000);
 
     }
@@ -40,42 +52,43 @@ class CountdownTimer {
 
 
 
-    getTimeComponents(t) {
+    getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+
+
+ }   
+
+
+
+
+
+
     
-        let days = this.pad(Math.floor(t / (1000 * 60 * 60 * 24)));
-        let hours = this.pad(Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        let mins = this.pad(Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)));
-        let secs = this.pad(Math.floor((t % (1000 * 60)) / 1000));
 
-        return ({ days, hours, mins, secs })
-    
-    
-    }
-
-
-    updateClockFace({ days, hours, mins, secs }) {
-
-
-        this.days.textContent = `${days}`;
-        this.hours.textContent = `${hours}`;
-        this.mins.textContent = `${mins}`;
-        this.secs.textContent = `${secs}`;
-     
-
-    };
-
-
-}
-
-
-
-
-const timer =  new CountdownTimer({
+const timer = new CountdownTimer({
+    onTick: updateClockFace,
   selector: '#timer-1',
   targetDate: new Date('Sep, 03, 2021'),
 });
     
 
+function updateClockFace({ days, hours, mins, secs }) {
+
+ this.selector = `${days}`
+        refs.days.textContent = `${days}`;
+        refs.hours.textContent = `${hours}`;
+        refs.mins.textContent = `${mins}`;
+        refs.secs.textContent = `${secs}`;
+
+    
+  };   
 
 
 
